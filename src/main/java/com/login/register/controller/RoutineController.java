490fa6d9f78999baba993b9e86d38d44b16e5962
Pritam Shrestha <p2ritamsthagrk@gmail.com>
2024-08-13 11:55:45 +0545

    Remove unused code

--- src/main/java/com/login/register/controller/RoutineController.java
+++ src/main/java/com/login/register/controller/RoutineController.java
@@ -1,7 +1,6 @@
 package com.login.register.controller;
 
 import com.login.register.Dto.RoutineDto;
-import com.login.register.model.Routine;
 import com.login.register.service.RoutineService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
